4fdce0837d5ca9ed6ec6b6d348b1934507242ad2
nitishpriyadarshi <nitish8152@gmail.com>
2022-08-07 16:39:42 +0530

    QuotientRemainder

---
+++ src/PackageBridgelab/com/QuotientRemainder.java
@@ -0,0 +1,26 @@
+package PackageBridgelab.com;
+
+
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        int dividend = 0,
+                divisor = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Dividend value: ");
+        dividend = sc.nextInt();
+
+        System.out.println("Enter Divisor value: ");
+        divisor = sc.nextInt();
+
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);;
+    }
+}
